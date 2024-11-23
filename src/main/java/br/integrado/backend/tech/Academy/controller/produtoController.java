983677fbcbd05a32b2da67390afed2beb0f5c900
GustavoRCG <161781547+GustavoRCG@users.noreply.github.com>
2024-11-22 21:49:21 -0300

    inserts e findById

---
+++ src/main/java/br/integrado/backend/tech/Academy/controller/produtoController.java
@@ -0,0 +1,2 @@
+package br.integrado.backend.tech.Academy.controller;public class produtoController {
+}
---
+++ src/main/java/br/integrado/backend/tech/Academy/repository/produtoRepository.java
@@ -0,0 +1,6 @@
+package br.integrado.backend.tech.Academy;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface produtoRepository extends JpaRepository<> {
+}
